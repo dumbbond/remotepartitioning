d4075cda40827a588088245a0dc77981eded7c32
Georgi <evilsoldier@abv.bg>
2018-08-16 16:39:14 +0300

    use BatchPreparedStatementSetter in the processor

--- src/main/java/remote/processor/KohlsBarcodeProcessor.java
+++ src/main/java/remote/processor/KohlsBarcodeProcessor.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.retry.annotation.CircuitBreaker;
 import org.springframework.retry.annotation.Recover;
@@ -14,14 +15,17 @@ import org.springframework.web.client.RestTemplate;
 import remote.pojo.KohlsBarcode;
 import remote.pojo.KohlsUser;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class KohlsBarcodeProcessor implements ItemProcessor<KohlsUser, KohlsUser> {
 
     @Value("${barcode.service.url}")
     private String barcodeServiceUrl;
 
-//    @Value("${spring.batch.database.update.statement}")
-//    private String updateStatement;
+    @Value("${spring.batch.database.update.statement}")
+    private String updateStatement;
 
     @Autowired
     private  RestTemplate restTemplate;
@@ -67,8 +71,21 @@ public class KohlsBarcodeProcessor implements ItemProcessor<KohlsUser, KohlsUser
 
     private void updateStatus(KohlsUser user) {
 
-        // will think how to optimise it
-       jdbcTemplate.batchUpdate("UPDATE KOHLS.LOYALTY_ACCOUNTS SET STATUS = '" +user.getStatus() +  "' WHERE ID = " + user.getId());
+        jdbcTemplate.batchUpdate(updateStatement, new BatchPreparedStatementSetter() {
+
+           public void setValues(PreparedStatement ps, int i) throws SQLException {
+
+               System.out.println("trying");
+                ps.setString(1, user.getStatus());
+                ps.setLong(2, user.getId());
+           }
+
+           public int getBatchSize() {
+               return 1;
+           }
+        });
+
+       //jdbcTemplate.batchUpdate("UPDATE KOHLS.LOYALTY_ACCOUNTS SET STATUS = '" +user.getStatus() +  "' WHERE ID = " + user.getId());
 
     }
 
